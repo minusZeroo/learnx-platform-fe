685bac9acdf9b1563ea25fc1e16be5b7057bb3c4
Paul Jideani <pcijideani@gmail.com>
2024-05-13 22:16:20 +0200

    landing page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import StudentDashboard from "./pages/StudentDashboard";
+import photo from './download.jpeg'
+// import './App.css'
 
 
 const App = () => {
   return (
       <Router>
-        <div className="App">
-          <Navbar />
-          <Routes>
-            <Route path="/studentdashboard" element={<StudentDashboard />} />
-          </Routes>
-        </div>
+          <div className="App">
+              <Navbar/>
+              <Routes>
+                  <Route path="/studentdashboard" element={<StudentDashboard/>}/>
+              </Routes>
+              <img
+                  src={photo}
+                  style={{
+                      position: 'fixed',
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%) scale(3)',
+                      zIndex: '-1', // Ensure the image is behind other content
+                  }}
+                  alt="logo"
+              />
+
+          </div>
+
       </Router>
   );
 };
