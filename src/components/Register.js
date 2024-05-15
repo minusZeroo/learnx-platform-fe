16404102920389b6f7dd8d3a654cb8fe32584b30
Paul Jideani <pcijideani@gmail.com>
2024-05-15 20:10:09 +0200

    async funcs

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
+import { useNavigate } from 'react-router-dom';
 import './form.css';
 
 const Register = () => {
--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -34,17 +34,21 @@ const Booking = () => {
 
     const fetchTutorialRequests = async () => {
         try {
-
+            const studentId = JSON.parse(localStorage.getItem('user')).userId;
+            const response = await axios.get(`http://localhost:8080/api/tutorial-requests/by-student/${studentId}`);
+            setTutorialRequests(response.data);
         } catch (error) {
-
+            console.error('Error fetching tutorial requests:', error);
         }
     };
 
     const fetchBookedTutorials = async () => {
         try {
-            
-        } catch (e) {
-            
+            const studentId = JSON.parse(localStorage.getItem('user')).userId;
+            const response = await axios.get(`http://localhost:8080/api/tutorial-bookings/by-student/${studentId}`);
+            setBookedTutorials(response.data);
+        } catch (error) {
+            console.error('Error fetching booked tutorials:', error);
         }
-    }
+    };
 }
